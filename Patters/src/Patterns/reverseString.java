1048cd8e34bca2d7667d88944c556dacfb824b5f
pdhama01 <pdhama01@192.168.2.8>
2024-06-18 12:33:05 +0530

    test

--- Patters/src/Patterns/reverseString.java
+++ Patters/src/Patterns/reverseString.java
@@ -4,7 +4,8 @@ public class reverseString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s="Pooja Sanjay Dhamale";
+		String s="Pooja";
+		
 		for(int i=s.length()-1;i>=0;i--)
 		{
 			System.out.println(s.charAt(i));
