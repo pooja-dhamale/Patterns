25f7c615e67eedf864081175c87be689194ad402
pdhama01 <pdhama01@192.168.2.8>
2024-06-18 12:30:00 +0530

    patterns project

---
+++ Patters/src/Patterns/reverseString.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class reverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Pooja Sanjay Dhamale";
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			System.out.println(s.charAt(i));
+		}
+
+	}
+
+}
